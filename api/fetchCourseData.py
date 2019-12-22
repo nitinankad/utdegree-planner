adf10594fdf7cc9ab854533240c7b3f2b9caadeb
Nitin Ankad <46038298+nitinankad@users.noreply.github.com>
2019-12-21 18:28:11 -0600

    Update course fetching function
    Adjusted course fetcher for Google Cloud Functions

--- api/fetchCourseData.py
+++ api/fetchCourseData.py
@@ -3,78 +3,74 @@ from bs4 import BeautifulSoup
 from flask import Flask, jsonify, abort
 from random import randint
 
-app = Flask(__name__)
+def fetch_courses(request):
+    # Function to clear out any trailing spaces and numbers from the end of a course name
+    def clean_text(name):
+        for i in range(len(name), 0, -1):
+            if name[i-1].isalpha():
+                return name[:i]
 
-# Function to clear out any trailing spaces and numbers from the end of a course name
-def clean_text(name):
-	for i in range(len(name), 0, -1):
-		if name[i-1].isalpha():
-			return name[:i]
+        return name
+    
+    result = []
 
-	return name
+    r = requests.get("https://catalog.utdallas.edu/2019-undergraduate/programs/ecs/computer-science/four-year")
+    html = r.text
 
-@app.route('/api/<int:task_id>', methods=['GET'])
-def get_task(task_id):
-	if task_id == 1:
-		result = []
+    soup = BeautifulSoup(html, "html.parser")
+    tables = soup.findAll("table")[:-1]
 
-		r = requests.get("https://catalog.utdallas.edu/2019-undergraduate/programs/ecs/computer-science/four-year")
-		html = r.text
+    for table in tables:
+        year_name = table.find("th").getText().split(" ")[0].upper()
 
-		soup = BeautifulSoup(html, "html.parser")
-		tables = soup.findAll("table")[:-1]
+        year = {}
+        year["id"] = str(randint(1000,9999))
+        year["year"] = year_name
+        year["semesters"] = []
 
-		for table in tables:
-			year_name = table.find("th").getText().split(" ")[0].upper()
+        fall, spring = {}, {}
 
-			year = {}
-			year["id"] = str(randint(1000,9999))
-			year["year"] = year_name
-			year["semesters"] = []
+        fall["id"] = str(randint(10000, 99999))
+        fall["content"] = "Fall semester"
+        fall["subItems"] = []
 
-			fall, spring = {}, {}
+        spring["id"] = str(randint(10000, 99999))
+        spring["content"] = "Spring semester"
+        spring["subItems"] = []
 
-			fall["id"] = str(randint(10000, 99999))
-			fall["content"] = "Fall semester"
-			fall["subItems"] = []
+        for row in table.find_all("tr")[2:]:
+            col = row.find_all("td")
 
-			spring["id"] = str(randint(10000, 99999))
-			spring["content"] = "Spring semester"
-			spring["subItems"] = []
+            if len(col) == 0: continue
 
-			for row in table.find_all("tr")[2:]:
-				col = row.find_all("td")
+            if len(col[0].getText()) != 0:
+                fall_class = {
+                    "id": str(randint(100000, 999999)),
+                    "content": clean_text(col[0].getText())
+                }
+                fall["subItems"].append(fall_class)
 
-				if len(col) == 0: continue
+            if len(col[2].getText()) != 0:
+                spring_class = {
+                    "id": str(randint(100000, 999999)),
+                    "content": clean_text(col[2].getText())
+                }
 
-				if len(col[0].getText()) != 0:
-					fall_class = {
-						"id": str(randint(100000, 999999)),
-						"content": clean_text(col[0].getText())
-					}
-					fall["subItems"].append(fall_class)
+                spring["subItems"].append(spring_class)
 
-				if len(col[2].getText()) != 0:
-					spring_class = {
-						"id": str(randint(100000, 999999)),
-						"content": clean_text(col[2].getText())
-					}
+        year["semesters"].append(fall)
+        year["semesters"].append(spring)
+        year["semesters"].append({
+            "id": str(randint(10000, 99999)),
+            "content": "Summer semester",
+            "subItems": []
+        })
 
-					spring["subItems"].append(spring_class)
+        result.append(year)
+        
+    response = jsonify(result)
+    response.headers.set('Access-Control-Allow-Origin', '*')
+    response.headers.set('Access-Control-Allow-Headers', 'Access-Control-Allow-Origin')
+    response.headers.set('Access-Control-Allow-Methods', 'GET, POST')
 
-			year["semesters"].append(fall)
-			year["semesters"].append(spring)
-			year["semesters"].append({
-				"id": str(randint(10000, 99999)),
-				"content": "Summer semester",
-				"subItems": []
-			})
-
-			result.append(year)
-
-		return jsonify(result)
-
-	return jsonify({"Error": "Invalid API request"})
-
-if __name__ == '__main__':
-	app.run(port=8000,debug=True)
+    return response
