1301723b5bb12dc885872a2935ceab23801fc483
nitinankad <nitin.ankad.340@gmail.com>
2020-01-03 14:58:48 -0600

    Added responsive drawer for mobile support

--- frontend/src/layout/Topbar.js
+++ frontend/src/layout/Topbar.js
@@ -1,19 +1,35 @@
 import React from "react";
-import { AppBar, Toolbar, Typography, makeStyles } from '@material-ui/core';
+import { AppBar, Toolbar, Typography, makeStyles, IconButton } from '@material-ui/core';
+import MenuIcon from "@material-ui/icons/Menu";
 
 const useStyles = makeStyles(theme => ({
   root: {
     zIndex: theme.zIndex.drawer + 1,
     backgroundColor: '#c75b12',
   },
+  menuIcon: {
+    marginRight: theme.spacing(1),
+    [theme.breakpoints.up("sm")]: {
+      display: 'none'
+    },
+  },
 }));
   
-const Topbar = () => {
+const Topbar = (props) => {
   const classes = useStyles();
+  const { onSidebarOpen } = props;
 
   return (
     <AppBar position="fixed" className={classes.root}>
       <Toolbar>
+        <IconButton
+          color="inherit"
+          onClick={onSidebarOpen}
+          className={classes.menuIcon}
+        >
+          <MenuIcon />
+        </IconButton>
+
         <Typography variant="h6" noWrap>
           UTDegree Planner
         </Typography>
